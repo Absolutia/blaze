2407993e5a7cb829c4c7e74eb06667a7109f868e
Absolutia <67517471+Absolutia@users.noreply.github.com>
2025-04-01 07:03:36 +0000

    Add files via upload

--- memloader.c
+++ memloader.c
@@ -9,18 +9,20 @@
 #define SWAP 2
 #define DISABLED 3
 
-size_t requested_memory[3];
-size_t available_memory[3];
-uint8_t prot_thresholds[3];
+size_t req_memory[3];
+size_t avail_memory[3];
+uint8_t prot_thresh[3];
 struct sysinfo info;
 
-struct loader_flags{
-	uint64_t memory_protection_type : 2;	//Free memory checks
-	uint64_t mmap : 1;			//Memory-map file (recommended for read-only files)
-	uint64_t stream : 1;			//Stream data into/out of memory
-};
+typedef struct{
+	size_t memory_protection_type : 2;	//Free memory checks
+	size_t mmap : 1;			//Memory-map file (recommended for read-only files)
+	size_t stream : 1;			//Stream data into/out of memory
+	size_t *object;				//Objects to be loaded
+	size_t *target;				//Target buffer (optional)
+} loader_t;
 
-struct meminfo{
+typedef struct{
 	size_t totalram;
 	size_t freeram;
 	size_t sharedram;
@@ -30,55 +32,49 @@ struct meminfo{
 	size_t freeswap;
 	size_t usedswap;
 	size_t freemem;
-};
+} mem_t;
 
-void get_memory_stats(struct sysinfo *info, struct meminfo *mem){
+void get_memory_stats(struct sysinfo *info, mem_t *mem, size_t avail[]){
 	sysinfo(info);
 
 	mem->totalram = info->totalram * info->mem_unit;
-	mem->freeram = info->freeram * info->mem_unit;
+	avail[RAM] = mem->freeram = info->freeram * info->mem_unit;
 	mem->sharedram = info->sharedram * info->mem_unit;
 	mem->bufferram = info->bufferram * info->mem_unit;
 	mem->totalswap = info->totalswap * info->mem_unit;
-	mem->freeswap = info->freeswap * info->mem_unit;
+	avail[SWAP] = mem->freeswap = info->freeswap * info->mem_unit;
 
 	mem->usedram = mem->totalram - mem->freeram;
 	mem->usedswap = mem->totalswap - mem->freeswap;
 
-	mem->freemem = mem->freeram + mem->freeswap;
+	avail[ALLMEMORY] = mem->freemem = mem->freeram + mem->freeswap;
 	mem->usedmem = mem->usedram + mem->usedswap;
 
 	return;
 }
 
-void pack_arrays(size_t available[], struct meminfo *mem){
-	available[ALLMEMORY] = mem->freemem;
-	available[RAM] = mem->freeram;
-	available[SWAP] = mem->freeswap;
-	return;
-}
-
-bool enforce_protection_thresholds(size_t requested[], size_t available[], uint8_t protection[], struct loader_flags *flags){
+bool enforceprot(size_t req[], size_t avail[], uint8_t prot[], loader_t *flags){
 	for(i = 0; i < 3; i++){
-		if(protection[i] > 100){
-			protection[i] = 100;
+		if(prot[i] > 100){
+			prot[i] = 100;
 		}
 	}
-	if(flags->memory_protection_type == ALLMEM){
-		for(i = 0; i < 3; i++){
-			if(requested[i] > available[i] * protection[i] / 100){
-				return true; //Protection tripped
+	switch(flags->mem_prot_type){
+		case ALLMEM:
+			for(i = 0; i < 3; i++){
+				if(req[i] > avail[i] * prot[i] / 100){
+					return true;
+				}
 			}
-		}
-	}else if(flags->memory_protection_type == RAM){
-		if(requested[RAM] > available[RAM] * protection[RAM] / 100){
-			return true;
-		}
-	}else if(flags->memory_protection_type == SWAP){
-		if(requested[SWAP] > available[SWAP] * protection[SWAP] / 100){
-			return true;
-		}
-	}else if(flags->memory_protection_type == DISABLED){
-		return false;
+		case RAM:
+			if(req[RAM] > avail[RAM] * prot[i] / 100){
+				return true;
+			}
+		case SWAP:
+			if(req[SWAP] > avail[SWAP] * prot[i] / 100){
+				return true;
+			}
+		case DISABLED:
+			return false;
 	}
 }
